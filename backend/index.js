10432e92f35736c198f24b228aadee668a4da83a
Hashit <raizadaharshit2004@gmail.com>
2024-08-10 09:57:35 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,41 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __fileName = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__fileName);
+dotenv.config();
+const app = express();
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+//file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+//MONGOOSE setup
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on Port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
