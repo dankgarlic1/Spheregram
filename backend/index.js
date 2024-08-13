a1471b50d3b29b0ee41ec73e8e47206cc43f2e67
Hashit <raizadaharshit2004@gmail.com>
2024-08-13 17:21:07 +0530

    dummy data added

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,9 @@ import { userRouter } from "./routes/users.js";
 import { postRouter } from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import { createPost } from "./controller/posts.js";
-
+import { users, posts } from "./data/index.js";
+import User from "./model/User.js";
+import Post from "./model/Post.js";
 const __fileName = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__fileName);
@@ -48,6 +50,10 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/posts", postRouter);
+
+//add dummy data, run this only once
+// User.insertMany(users);
+// Post.insertMany(posts);
 
 //MONGOOSE setup
 const PORT = process.env.PORT || 6001;
