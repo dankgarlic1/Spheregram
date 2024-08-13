797dfb6ddb3e7c79685510d4d218226acfd2990e
Hashit <raizadaharshit2004@gmail.com>
2024-08-13 16:41:59 +0530

    post route done

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,3 +1,9 @@
 import express from "express";
+import { createPost, getUserPost, likePost } from "../controller/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
 export const postRouter = express.Router();
+
+postRouter.post("/", verifyToken, createPost);
+postRouter.get("/:id/posts", verifyToken, getUserPost);
+postRouter.patch("/:id/like", verifyToken, likePost);
