e5e6d83efb9357b5b225b3b76f74616110713356
Hashit <raizadaharshit2004@gmail.com>
2024-08-13 13:45:50 +0530

    User routes completed

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -10,6 +10,56 @@ export const getUser = async (req, res) => {
     return res.status(200).json({ msg: "User found successfully", user });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: error.message });
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const getUserFriend = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, location, occupation, picturePath }) => {
+        return { _id, firstName, lastName, location, occupation, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+    if (user.friends.includes(friendId)) {
+      //remove
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      //add
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, location, occupation, picturePath }) => {
+        return { _id, firstName, lastName, location, occupation, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
   }
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { getUser } from "../controller/users.js";
+import {
+  addRemoveFriend,
+  getUser,
+  getUserFriend,
+} from "../controller/users.js";
 export const userRouter = express.Router();
 
 userRouter.get("/:id", verifyToken, getUser);
+userRouter.get("/:id/friends", verifyToken, getUserFriend);
+userRouter.patch("/:id/:friendId", verifyToken, addRemoveFriend);
