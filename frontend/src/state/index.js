10aa3023af289c97892389aa365f4c4387f382c3
Hashit <raizadaharshit2004@gmail.com>
2024-08-16 17:23:09 +0530

    ok

---
+++ frontend/src/state/index.js
@@ -0,0 +1,48 @@
+import { asyncThunkCreator, createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.log("User friends non-existent ");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPost = state.posts.map((post) => {
+        if (post._id === action.payload._id) {
+          return action.payload.post;
+        }
+        return post;
+      });
+      state.posts = updatedPost;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setPost, setPosts, setFriends } =
+  authSlice.actions;
+export default authSlice.reducer;
