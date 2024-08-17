7566b1522e4bcbdb6c0bd566ba10c7df5184942c
Hashit <raizadaharshit2004@gmail.com>
2024-08-17 07:46:59 +0530

    ok

--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -1,3 +1,21 @@
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FlexBetween } from "../../components/FlexBetween";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+  return <FlexBetween padding="1rem 6%" backgroundColor={alt}></FlexBetween>;
 };
