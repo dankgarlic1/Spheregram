16e28fafc2e57e1636932823010f491026e5b5f7
Hashit <raizadaharshit2004@gmail.com>
2024-08-28 14:59:50 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./scenes/loginPage";
 import { HomePage } from "./scenes/homePage";
 import { ProfilePage } from "./scenes/profilePage";
@@ -11,6 +11,7 @@ import { CssBaseline } from "@mui/material";
 function App() {
   const mode = useSelector((state) => state.mode); //grabs initial state
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
@@ -18,8 +19,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
